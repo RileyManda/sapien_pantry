1609cb19dfa6962cc2d61bef672cdb2b6ada78e2
RileyManda <rileymanda69@gmail.com>
2023-04-04 12:54:15 +0200

    cleanup

--- lib/view/app_drawer.dart
+++ lib/view/app_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sapienpantry/utils/constants.dart';
 import 'package:sapienpantry/view/category_view.dart';
+import 'package:sapienpantry/utils/messages.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({
@@ -19,25 +20,28 @@ class AppDrawer extends StatelessWidget {
             color: pPrimaryColor,
             // padding: const EdgeInsets.all(16.0),
             padding: EdgeInsets.zero,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                const Icon(
-                  Icons.person,
-                  size: 60,
-                  color: Colors.white,
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  '${authController.user!.email}',
-                  style: const TextStyle(
+            child: Center(
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                // mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.person,
+                    size: 60,
                     color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
                   ),
-                ),
-              ],
+                  const SizedBox(height: 10),
+                  Text(
+                    '${authController.user!.email}',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           ListTile(
@@ -116,11 +120,7 @@ class AppDrawer extends StatelessWidget {
                                   deletionInProgress = true;
                                 });
                                 await pantryController.deleteCompleted();
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: const Text('Items deleted successfully'),
-                                  ),
-                                );
+                                deleteCompleted(context);
                                 Navigator.of(context).pop();
                               }
                             },
